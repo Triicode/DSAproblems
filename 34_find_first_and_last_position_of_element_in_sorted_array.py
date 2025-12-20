7f5d220c27e5152fd7d865c939299235b688834b
Triicode <101192546+Triicode@users.noreply.github.com>
2025-12-21 01:45:36 +0530

    Update 34_find_first_and_last_position_of_element_in_sorted_array.py - LeetCode Problem 34

--- 34_find_first_and_last_position_of_element_in_sorted_array.py
+++ 34_find_first_and_last_position_of_element_in_sorted_array.py
@@ -10,21 +10,29 @@ class Solution(object):
         right = length - 1
         initial = -1
         final = -1
-        n = length//2
         if length == 0:
             return [initial,final]
         while left<=right:
-             
             n = left + (right-left)//2
             if nums[n]<target:
                 left = n+1
             elif nums[n]>target:
                 right = n-1
             elif nums[n]==target:
-                initial = final = n
-                while initial>=1 and nums[initial - 1] == target:
-                    initial-=1
-                while final<length-1 and nums[final + 1] == target:
-                    final+=1
+                mid = r1 = n
+                while left<=r1:
+                    n = left + (r1 - left)//2
+                    if nums[n]<target:
+                        left = n+1
+                    else:
+                        r1 = n-1
+                while mid<=right:
+                    n = mid + (right - mid)//2
+                    if nums[n]>target:
+                        right = n-1
+                    else:
+                        mid = n + 1
+                initial = left
+                final = right
                 break
         return [initial,final]
